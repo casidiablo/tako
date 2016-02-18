53057fadb97815a256f321899007927b5a3c76c3
Cristian <me@cristian.io>
2016-02-18 08:45:02 -0800

    Move declaration of GH token and Slack URL outside handler fn

--- src/tako/core.clj
+++ src/tako/core.clj
@@ -5,15 +5,21 @@
   (:gen-class
    :methods [^:static [handler [Object] String]]))
 
-(defn -handler [unused]
-  (info (str "Called tako from lambda... " unused))
-  (binding [tako.github/github-token "herewego"]
+(def github-token      "PLACE YOUR GITHUB TOKEN HERE")
+(def slack-webhook-url "PLACE THE SLACK WEBHOOK URL HERE")
+
+(defn -handler
+  "This is the function called by AWS Lambda"
+  [unused]
+  (info (str "Called tako from lambda... "))
+  (binding [tako.github/github-token github-token
+            tako.slack/slack-webhook-url slack-webhook-url]
     (slack/publish-notifications (github/poll-notifications))))
 
 
 (defn -main
   "This function is included for testing purposes. It is NOT used by AWS lambda.
-  It is intended to be used with `lein run your-github-token`"
+  It is intended to be used with `lein run your-github-token slack-url`"
   [& args]
   (info "Running tako from main...")
   (binding [tako.github/github-token (first args)
